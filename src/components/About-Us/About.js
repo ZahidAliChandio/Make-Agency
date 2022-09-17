54bf83a59917604b48725e85a3448d0b66b33d8b
Zahid Ali <zahidalichandio240@gmail.com>
2022-09-17 13:30:31 +0500

    Arrowed Image and About us team images Modified

--- src/components/About-Us/About.js
+++ src/components/About-Us/About.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import './About.css'
 import ArrowedImage from '../General/ArrowedImage'
 import ContactHomgBg from '../../images/about-bg-image.jpg'
 import List from './List'
@@ -7,16 +6,24 @@ import Team from './Team'
 import ProjectHire from '../General/ProjectHire'
 import Articles from '../LandingSection/Articles'
 import Reviews from '../LandingSection/Reviews'
+import './About.css'
 
 export default function About() {
   return (
-    <div id="contact-section">
-      <ArrowedImage image={ContactHomgBg} title={'A young diverse team of digital experts'} subtitle={'With offices in the heart of London’s creative community, we’ve got our finger on the pulse.'} />
-      <List/>
-      <Team/>
-      <ProjectHire/>
-      <Articles bgColor={'#fff'}/>
-      <Reviews/>
+    <div id="about-section">
+      <div className="arrow-image-container">
+        {/* <ArrowedImage
+                        bgImage={IntroBgImage}
+                        title={'Commercially Creative'}
+                        subtitle={'Make is a London-based digital agency. We guarantee results across three key business areas: web design & build, social media & digital marketing, and creative content.'}
+                        Anchor={'Explore'} /> */}
+        <ArrowedImage bgImage={ContactHomgBg} title={'A young diverse team of digital experts'} subtitle={'With offices in the heart of London’s creative community, we’ve got our finger on the pulse.'} />
+      </div>
+      <List />
+      <Team />
+      <ProjectHire />
+      <Articles bgColor={'#fff'} />
+      <Reviews />
     </div>
   )
 }
